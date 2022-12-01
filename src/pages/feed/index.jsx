25394c02cbde36c03aac74de00305daf5b1fd13d
lojaonline <75400381+AlessandroRossiUFF@users.noreply.github.com>
2022-12-01 21:36:09 +0000

    Initial commit

--- src/pages/feed/index.jsx
+++ src/pages/feed/index.jsx
@@ -25,22 +25,22 @@ const Feed = () => {
         <Column flex={1}>
           <TitleHighlight> # RANKING 5 TOP DA SEMANA </TitleHighlight>
           <UserInfo
-            nome="Pablo Henrique"
+            nome="Alessandro Rossi"
             image="https://avatars.githubusercontent.com/u/45184516?v="
             percentual={25}
           />
           <UserInfo
-            nome="Pablo Henrique"
+            nome="Alessandro Rossi"
             image="https://avatars.githubusercontent.com/u/45184516?v="
             percentual={65}
           />
           <UserInfo
-            nome="Pablo Henrique"
+            nome="Alessandro Rossi"
             image="https://avatars.githubusercontent.com/u/45184516?v="
             percentual={45}
           />
           <UserInfo
-            nome="Pablo Henrique"
+            nome="Alessandro Rossi"
             image="https://avatars.githubusercontent.com/u/45184516?v="
             percentual={72}
           />
